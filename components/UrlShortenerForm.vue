8d5ee23c3f59b4ae506fe5ba3af4b22388b1113e
gkumar9891 <gauravkumargk9892@gmail.com>
2024-09-27 23:55:39 +0530

    added form

---
+++ components/UrlShortenerForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <form class="xl:w-1/3 mx-auto">
+        <div class="flex mb-4 border-solid border-2 rounded-md overflow-hidden">
+            <span class="py-4 px-6 bg-blue-500 text-white">URL</span>
+            <input type="text" class="flex-1 px-3" placeholder="paste your url" aria-label="url field" aria-describedby="paste your url here" /> 
+        </div>
+        <div class="text-center">
+            <button type="submit" class="py-3 px-3 bg-blue-500 text-white rounded-md transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-110 transition ease-in-out duration-300">Get short link</button>
+        </div>
+    </form>
+</template>
+ 
+<script setup lang="ts">
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <section class="section pt-40 pb-40">
+        <div class="text-center mb-5">
+            <h1 class="text-3xl">URL Shortener</h1>
+        </div>
+        <UrlShortenerForm></UrlShortenerForm>
+    </section>
+</template>
